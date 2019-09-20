448f71a9dfb87501efc3494fbdd870d7da495b37
Николай <kazlou.mikalai.s@gmail.com>
2019-09-20 17:47:45 +0300

    first task, homework two

---
+++ Task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 10;
+            int b = 5;
+            short c = 6;
+            long d = 2;
+            double e = 11.2;
+            char f = 'A';
+
+            //3 примера неявного преобразования типов
+            c = a;
+            b = c;
+            d = a;
+
+            //3 примера явного преобразования типов
+            e = (double) a;
+            f = (char) c;
+            e = (double)d;
+
+            //1 пример операции упаковки
+            object up = a;
+
+            //1 пример операции распаковки
+            up = 111;
+            int a1 = (int) up;
+        }
+    }
+}
