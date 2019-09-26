2cce0e3b16b460c64f4a401c40d80e17d954a12d
Николай <kazlou.mikalai.s@gmail.com>
2019-09-26 12:28:21 +0300

    my homework3.4

---
+++ HW_03/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int ReadConvert()
+        {
+            Console.WriteLine("Введите число № 1 и нажмите кнопку - <Enter>");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Введите число № 2 и нажмите кнопку - <Enter>");
+            string number2 = Console.ReadLine();
+            int number11 = Convert.ToInt32(number1);
+            int number12 = Convert.ToInt32(number2);
+            Console.WriteLine("Введите оператор \"+\" или \"-\"");
+            string operators = Console.ReadLine();
+            
+            if (operators == "+" ) {
+                int resalt = number11 + number12;
+                return resalt;}
+            else 
+            {
+                int resalt = number11 - number12;
+                return resalt;
+            }
+          
+        }
+        static void Main(string[] args)
+        {
+            int ResaltEnd = ReadConvert();
+            Console.WriteLine("Какой результат вычисления?");
+            string numberAnswer = Console.ReadLine();
+            int answer = Convert.ToInt32(numberAnswer);
+            if (answer == ResaltEnd) Console.WriteLine("Правильно!");
+            else if (answer < ResaltEnd) Console.WriteLine("Неправильно!\n\nДолжно быть больше!");
+            else Console.WriteLine("Неправильно!\nДолжно быть  меньше!");
+        }
+    }
+}
