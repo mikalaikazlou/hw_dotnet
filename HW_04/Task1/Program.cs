00ca0f409e9c194f7f54400c6b940d5cf3172ebe
Николай <kazlou.mikalai.s@gmail.com>
2019-10-01 00:09:06 +0300

    my homework 4

---
+++ HW_04/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i > 64; i--)
+            {
+                string upperChar = Char.ConvertFromUtf32(i);
+                Console.Write(upperChar + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 122; i > 96; i--)
+            {
+                string lowerChar = Char.ConvertFromUtf32(i);
+                Console.Write(lowerChar + " ");
+            }
+        }
+    }
+}
---
+++ HW_04/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 1000;
+            double percent = 0.02;
+            double increase;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                increase = sum * percent;
+                sum += increase;
+                Console.WriteLine("Прирост за " + i + " месяц равен " + "{0:#.## руб.}", increase);
+            }
+
+            for (int i = 1; i <= 12; i++)
+            {
+                sum += sum * percent;
+                if (i >= 3 && i <= 12)
+                {
+                 Console.WriteLine("Сумма вклада через " + i + " месяца(-ев) равна " + "{0:#.## руб.}", sum);
+                }
+            }
+        }
+    }
+}
