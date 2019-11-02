539ab4304e96359e278868639664841e052e7a0c
Николай <kazlou.mikalai.s@gmail.com>
2019-11-02 15:35:06 +0300

    My new homework number 9 with MC SQL Server

---
+++ HW_09(with_SQL)/HW_09(new_with_SQL)/IMotocycleRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_09_new_with_SQL_
+{
+    interface IMotocycleRepository
+    {
+        void CreateMotocycle(Motocycle moto);
+
+        Motocycle GetMotocycle(int id);
+
+        void UpdateMotoCycleModel(int id, string model);
+
+        void UpdateMotoCycleName(int id, string name);
+
+        void UpdateMotoCycleOdometer(int id, int odometer);
+
+        void UpdateMotoCycleYear(int id, int year);
+
+        void DeleteMotocycle(int id);
+    }
+}
